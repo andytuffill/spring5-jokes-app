08fc2df72c170d283aa59494d79e9042b1dec942
Andy Tuffill <andy.tuffill@gmail.com>
2018-10-22 21:16:08 +0100

    Updating requestmapping to use multiple paths

--- src/main/java/com/example/springbootjokesapp/controller/JokesController.java
+++ src/main/java/com/example/springbootjokesapp/controller/JokesController.java
@@ -10,29 +10,16 @@ public class JokesController {
 
     private JokeService jokeService;
 
-    @RequestMapping("/")
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
     public String getJokeWithPath(Model model){
-        return getJoke(model);
-    }
-
-    @RequestMapping("")
-    public String getJokeWithNoPath(Model model){
-        return getJoke(model);
-    }
-
-
-    private String getJoke(Model model){
-        model.addAttribute("joke", getJokeService().getJoke());
+        model.addAttribute("joke", jokeService.getJoke());
 
         return "chucknorris";
     }
 
 
-    public JokesController(JokeService jokeService) {
-        this.jokeService = jokeService;
-    }
-
-    public JokeService getJokeService() {
-        return jokeService;
-    }
 }
