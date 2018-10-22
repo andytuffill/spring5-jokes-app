af85b8bf91acd79698e69062ca069ab47cd43197
Andy Tuffill <andy.tuffill@gmail.com>
2018-10-22 21:20:16 +0100

    tidying up controller to use annotation, plus using improved method name

--- src/main/java/com/example/springbootjokesapp/controller/JokesController.java
+++ src/main/java/com/example/springbootjokesapp/controller/JokesController.java
@@ -1,6 +1,7 @@
 package com.example.springbootjokesapp.controller;
 
 import com.example.springbootjokesapp.service.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +11,13 @@ public class JokesController {
 
     private JokeService jokeService;
 
+    @Autowired
     public JokesController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
 
     @RequestMapping({"/",""})
-    public String getJokeWithPath(Model model){
+    public String showJoke(Model model){
         model.addAttribute("joke", jokeService.getJoke());
 
         return "chucknorris";
